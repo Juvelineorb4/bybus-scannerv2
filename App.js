520b6d857bff9dfac84c362de11a85f3e2fa1db3
Juvelineorb4 <73950715+Juvelineorb4@users.noreply.github.com>
2023-10-17 14:16:54 -0500

    cambios

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import Navigation from "@/routes/Navigation";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import { RecoilRoot } from "recoil";
+// exportaciones amplify
+import { Amplify } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
 
+// Configuracion de Amplify
+Amplify.configure(awsconfig);
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    thin: require("@/utils/fonts/Montserrat-Thin.ttf"),
+    regular: require("@/utils/fonts/Montserrat-Regular.ttf"),
+    light: require("@/utils/fonts/Montserrat-Light.ttf"),
+    bold: require("@/utils/fonts/Montserrat-Bold.ttf"),
+    extralight: require("@/utils/fonts/Montserrat-ExtraLight.ttf"),
+    medium: require("@/utils/fonts/Montserrat-Medium.ttf"),
+    black: require("@/utils/fonts/Montserrat-Black.ttf"),
+    semibold: require("@/utils/fonts/Montserrat-SemiBold.ttf"),
+    thinItalic: require("@/utils/fonts/Montserrat-ThinItalic.ttf"),
+    mediumItalic: require("@/utils/fonts/Montserrat-MediumItalic.ttf"),
+    lightItalic: require("@/utils/fonts/Montserrat-LightItalic.ttf"),
+    boldItalic: require("@/utils/fonts/Montserrat-BoldItalic.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <RecoilRoot>
+          <BottomSheetModalProvider>
+            <Navigation />
+          </BottomSheetModalProvider>
+        </RecoilRoot>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
